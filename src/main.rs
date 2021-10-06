8d042cf1418fd4c72ba44493a4d263ff5680d88e
rvcas <x@rvcas.dev>
2021-10-05 23:01:53 -0400

    feat: parse input files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
+
+use anyhow::{anyhow, Context, Result};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -8,7 +10,93 @@ struct Opt {
     input: PathBuf,
 }
 
-fn main() {
+#[derive(Debug)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug)]
+struct Move {
+    initial: Position,
+    destination: Position,
+    line: usize,
+    src: String,
+}
+
+fn main() -> Result<()> {
     let opt = Opt::from_args();
-    println!("{:#?}", opt);
+
+    let path = opt.input.as_path();
+
+    let mut input = fs::read_to_string(path)
+        .with_context(|| format!("failed to read moves from {}", path.display()))?;
+
+    if let Some('\n') = input.chars().next_back() {
+        input.pop();
+    }
+
+    if let Some('\r') = input.chars().next_back() {
+        input.pop();
+    }
+
+    let moves = parse_moves(&input)?;
+
+    println!("{:?}", moves);
+
+    Ok(())
+}
+
+fn parse_moves(input: &str) -> Result<Vec<Move>> {
+    input
+        .split("\n")
+        .enumerate()
+        .fold(Ok(vec![]), |acc, (index, line)| {
+            let move_line = line
+                .split(",")
+                .map(|n| {
+                    let mut n = n.to_string();
+
+                    if let Some('\r') = n.chars().next_back() {
+                        n.pop();
+                    }
+
+                    Ok(n.parse::<i32>().with_context(|| {
+                        format!(
+                            "line {}: failed to parse move {} because {} is not a number",
+                            index + 1,
+                            line,
+                            n
+                        )
+                    })?)
+                })
+                .collect::<Result<Vec<i32>>>()?;
+
+            if move_line.len() == 4 {
+                let mut acc = acc?;
+
+                let initial = Position {
+                    x: move_line[0],
+                    y: move_line[1],
+                };
+
+                let destination = Position {
+                    x: move_line[2],
+                    y: move_line[3],
+                };
+
+                acc.push(Move {
+                    initial,
+                    destination,
+                    line: index + 1,
+                    src: line.to_string(),
+                });
+
+                Ok(acc)
+            } else {
+                Err(anyhow!(
+                    "line {}: failed to parse move {} because it is invalid"
+                ))
+            }
+        })
 }
