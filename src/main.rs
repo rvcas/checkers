95c8b7431832db691ea09d9c524c234838a43d8e
rvcas <x@rvcas.dev>
2021-10-05 21:46:37 -0400

    feat: use structopt to capture input file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "checkers", about = "Valid checker's moves")]
+struct Opt {
+    #[structopt(name = "FILE", parse(from_os_str))]
+    input: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:#?}", opt);
 }
