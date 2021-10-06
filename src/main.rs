105f5a901f4f46a35c64d4fe80e381732cb64312
rvcas <x@rvcas.dev>
2021-10-06 11:15:41 -0400

    feat: add tests, helpers, and input files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{fs, path::PathBuf};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 use anyhow::{anyhow, Context, Result};
 use structopt::StructOpt;
@@ -27,11 +30,31 @@ struct Move {
 fn main() -> Result<()> {
     let opt = Opt::from_args();
 
-    let path = opt.input.as_path();
+    let mut input = read_file(&opt.input)?;
 
-    let mut input = fs::read_to_string(path)
-        .with_context(|| format!("failed to read moves from {}", path.display()))?;
+    let answer = validate_input(&mut input, &opt.input)?;
 
+    println!("{}", answer);
+
+    Ok(())
+}
+
+fn read_file(path: &Path) -> Result<String> {
+    fs::read_to_string(path)
+        .with_context(|| format!("failed to read moves from {}", path.display()))
+}
+
+fn validate_input(input: &mut String, path: &Path) -> Result<String> {
+    clean_input(input);
+
+    let moves = parse_moves(&input)?;
+
+    println!("{:?}", moves);
+
+    Ok(format!("{} - ", path.display()))
+}
+
+fn clean_input(input: &mut String) {
     if let Some('\n') = input.chars().next_back() {
         input.pop();
     }
@@ -39,33 +62,25 @@ fn main() -> Result<()> {
     if let Some('\r') = input.chars().next_back() {
         input.pop();
     }
-
-    let moves = parse_moves(&input)?;
-
-    println!("{:?}", moves);
-
-    Ok(())
 }
 
 fn parse_moves(input: &str) -> Result<Vec<Move>> {
     input
         .split("\n")
         .enumerate()
-        .fold(Ok(vec![]), |acc, (index, line)| {
-            let move_line = line
+        .fold(Ok(vec![]), |acc, (index, input_line)| {
+            let move_line = input_line
                 .split(",")
                 .map(|n| {
                     let mut n = n.to_string();
 
-                    if let Some('\r') = n.chars().next_back() {
-                        n.pop();
-                    }
+                    clean_input(&mut n);
 
                     Ok(n.parse::<i32>().with_context(|| {
                         format!(
                             "line {}: failed to parse move {} because {} is not a number",
                             index + 1,
-                            line,
+                            input_line,
                             n
                         )
                     })?)
@@ -89,14 +104,62 @@ fn parse_moves(input: &str) -> Result<Vec<Move>> {
                     initial,
                     destination,
                     line: index + 1,
-                    src: line.to_string(),
+                    src: input_line.to_string(),
                 });
 
                 Ok(acc)
             } else {
                 Err(anyhow!(
-                    "line {}: failed to parse move {} because it is invalid"
+                    "line {}: failed to parse move {} because it is invalid",
+                    index + 1,
+                    input_line
                 ))
             }
         })
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use crate::validate_input;
+
+    #[test]
+    fn red() {
+        let mut input = include_str!("../inputs/red.txt").to_string();
+
+        let answer = validate_input(&mut input, &PathBuf::from("red.txt"));
+
+        assert_eq!(answer.unwrap().as_str(), "red.txt - red");
+    }
+
+    #[test]
+    fn white() {
+        let mut input = include_str!("../inputs/white.txt").to_string();
+
+        let answer = validate_input(&mut input, &PathBuf::from("white.txt"));
+
+        assert_eq!(answer.unwrap().as_str(), "white.txt - white");
+    }
+
+    #[test]
+    fn illegal_move() {
+        let mut input = include_str!("../inputs/illegal_move.txt").to_string();
+
+        let answer = validate_input(&mut input, &PathBuf::from("illegal_move.txt"));
+
+        assert_eq!(
+            answer.unwrap().as_str(),
+            "illegal_move.txt - line 15 illegal move: 1,0,0,5"
+        );
+    }
+
+    #[test]
+    fn incomplete() {
+        let mut input = include_str!("../inputs/incomplete.txt").to_string();
+
+        let answer = validate_input(&mut input, &PathBuf::from("incomplete.txt"));
+
+        assert_eq!(answer.unwrap().as_str(), "incomplete.txt - incomplete game");
+    }
 }
